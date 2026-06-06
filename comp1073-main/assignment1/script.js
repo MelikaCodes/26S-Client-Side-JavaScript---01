7b36b83b3d0d2746d87a8e33b843998806da3833
kashefinterior-tech <kashef.interior@gmail.com>
2026-06-05 22:06:36 -0400

    where array

--- comp1073-main/assignment1/script.js
+++ comp1073-main/assignment1/script.js
@@ -24,3 +24,15 @@ var whatWords = [
   "landed on an uncharted moon",
   "outsmarted a rival crew"
 ];
+
+// Where Array
+var whereWords = [
+  "on the dark side of Mars",
+  "inside a drifting space station",
+  "near the rings of Saturn",
+  "on a frozen alien planet",
+  "deep inside a nebula",
+  "aboard a cloaked spacecraft",
+  "at the edge of the solar system",
+  "inside an abandoned asteroid base"
+];
