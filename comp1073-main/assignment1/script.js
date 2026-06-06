1a5dcf45f29c6fa1a97354fb7f604e099ab16fbd
kashefinterior-tech <kashef.interior@gmail.com>
2026-06-05 21:41:10 -0400

    word array

---
+++ comp1073-main/assignment1/script.js
@@ -0,0 +1,14 @@
+//🚀
+
+//Word Array
+
+var whoWord = [
+    "A fearless astronaut",
+    "A rookie space cadet",
+    "A mysterious alien",
+    "The captain of a starship",
+    "A lone space ranger",
+    "An android explorer",
+    "A rebel pilot",
+    "A young cosmonaut"
+]
