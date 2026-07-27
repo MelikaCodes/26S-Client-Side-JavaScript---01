add3502ce8dcf71fc0527ffe5b673114ceae261a
kashefinterior-tech <kashef.interior@gmail.com>
2026-07-26 21:47:06 -0400

    class pizza

--- comp1073-main/assignment2/js/script.js
+++ comp1073-main/assignment2/js/script.js
@@ -5,3 +5,21 @@
 const studentId = "200652992";
 const studentName = "Melika Kashef";
 
+document.getElementById("student-info").textContent =
+  `Student: ${studentName} | ID: ${studentId}`;
+
+
+  /* the Pizza class */
+  class Pizza{
+    constructor(name, size, crust, sause, cheeselevel, toppings, quantity, delivery, notes){
+        this.name= name;
+        this.size=size;
+        this.crust= crust;
+        this.sauce= sause;
+        this.cheeselevel= cheeselevel;
+        this.toppings= toppings;
+        this.quantity= quantity;
+        this.delivery= delivery;
+        this.notes= notes;
+    }
+  }
